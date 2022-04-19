d9b23481c5d3dd3eb657bf9abd4b781b12d683d8
favourpro <favourapps17@gmail.com>
2022-04-19 15:43:09 -0700

    added first files and import identifiers for the project

---
+++ _printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <strarg.h>
+/**
+ * _printf - check if theres is a valid format specifier
+ * specifier
+ * @format: possible format specifier
+ * Return: pointer to valid function or NULL
+ */
+
+int _printf(const char *format, ...);
+char* convert(unsigned int, int);
+
+int _printf(const char *format, ...)
+{
+	
+}
